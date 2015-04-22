0a83978d46c8c4034135748badc83772e9ba50fe
Aaron Cremona <dubbacremona@gmail.com>
2015-04-22 14:19:36 -0700

    formatting variables into strings

---
+++ ex05.rb
@@ -0,0 +1,16 @@
+my_name = "Your mom"
+my_age = 100
+my_height = 48
+my_weight = 320
+my_eyes = 'black'
+my_teeth = 'yellow'
+my_hair = "multicolored"
+
+puts "Let's talk about #{my_name}"
+puts "She's #{my_height} inches tall"
+puts "She's #{my_weight} pounds heavy"
+puts "Actually, that's not too heavy"
+puts "She's got #{my_eyes} eyes and #{my_hair} hair"
+puts "Her teeth are usually #{my_teeth} depending on coffee"
+
+puts "If I add #{my_weight}, #{my_height} and #{my_age} I get #{my_height + my_weight + my_age}"
