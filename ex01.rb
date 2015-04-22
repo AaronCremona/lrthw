c915dbebb93a5f4043d38c1c914e61eaa232c33a
Aaron Cremona <dubbacremona@gmail.com>
2015-04-21 17:33:44 -0700

    puts command

---
+++ ex01.rb
@@ -0,0 +1,6 @@
+puts "Hello World"
+puts "Hello Again"
+puts "I like typing this"
+puts "No I don't, I'm lying right now."
+puts "This is too repetitive. Or 'petitive."
+puts 'I "said" I really do not like this.'
