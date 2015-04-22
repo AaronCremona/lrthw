7b2c64dba09d674c6b8526ccc5c2dc18eea6bfbd
Aaron Cremona <dubbacremona@gmail.com>
2015-04-21 17:54:46 -0700

    octothrope # pow pow comment

---
+++ ex02.rb
@@ -0,0 +1,10 @@
+# A comment. This is so you can read your program later
+# I get it already, I don't think I need to repeat this
+# this is the same as python 
+
+puts "I could have code like this" # and the comment after is ignored
+
+# You can also use comments to disable or comment out a piece of code:
+# puts "This won't run"
+
+puts "Ths will run"
