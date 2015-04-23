9034aac5bc779d8cd4d3c5b406d62bafd2365cca
Aaron Cremona <dubbacremona@gmail.com>
2015-04-22 17:07:19 -0700

    line break and triple quotes

---
+++ ex09.rb
@@ -0,0 +1,12 @@
+days = "Mon Tues Wed Thurs Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApril\nMay\nI get it already"
+
+puts "here are the days #{days}"
+puts "here are the months #{months}"
+
+puts """
+Something weird is going on here.
+With three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5 or 6.
+"""
